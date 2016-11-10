ddb54616db77e50cde7fed2bf5aeada6f9623e7d
Elton Luis Kolling <eltonlk@gmail.com>
2016-11-10 15:33:41 -0200

    Service LocalStorage.

---
+++ resources/assets/app/js/services/localStorage.js
@@ -0,0 +1,21 @@
+export default {
+    get (key, defaultValue) {
+        return window.localStorage.getItem(key) || defaultValue;
+    },
+    getObject (key) {
+        return JSON.parse(window.localStorage.getItem(key) || null);
+    },
+    remove (key) {
+        return window.localStorage.removeItem(key);
+    },
+    set (key, value) {
+        window.localStorage.setItem(key, value);
+
+        return this.get(key);
+    },
+    setObject (key, object) {
+        window.localStorage.setItem(key, JSON.stringify(object));
+
+        return this.getObject(key);
+    }
+};

d4722b5a40614a789cc414755ad74a17e3e44790
Elton Luis Kolling <eltonlk@gmail.com>
2017-01-09 16:40:24 -0200

    Exibindo os errors de validação ao criar um banco.

--- resources/views/admin/banks/_form.blade.php
+++ resources/views/admin/banks/_form.blade.php
@@ -1,6 +1,9 @@
+@include('errors._error_field')
+
 <div class="row">
     <div class="input-field col s6">
-        {!! Form::label('name', 'Nome') !!}
-        {!! Form::text('name', null) !!}
+        <?php $nameHtml = $errors->first('name')? [ 'class' => 'validate invalid' ] : [] ?>
+        {!! Form::text('name', null, $nameHtml) !!}
+        {!! Form::label('name', 'Nome', [ 'data-error' => $errors->first('name') ]) !!}
     </div>
 </div>
